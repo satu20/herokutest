8eb39c8a7413fc7d8a87cb9a281846cfd435a70a
satu20 <satu.k.voutilainen@gmail.com>
2021-02-14 15:36:40 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello heroku!"
+
+    if __name__ == '__main__':
+        app.run(threaded=True, port=5000)
